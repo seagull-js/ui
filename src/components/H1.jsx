573a23e543a4ce8dd88042d9ee8bf084b02c5ea7
Maximilian Stroh <Hisako1337@gmail.com>
2018-03-03 21:52:50 +0100

    missing PropType Comment

--- src/components/H1.jsx
+++ src/components/H1.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 const H1 = ({ children }) => <h1>{children}</h1>
 
 H1.propTypes = {
+  /** text or child element. shall not contain block-style elements */
   children: PropTypes.node.isRequired,
 }
 
