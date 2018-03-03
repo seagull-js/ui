6678f619aaa7a335fb4b98be468d5bdb2ec0b8a1
Maximilian Stroh <Hisako1337@gmail.com>
2018-03-03 21:27:49 +0100

    added propTypes and knobs for playing around

--- src/components/H1.jsx
+++ src/components/H1.jsx
@@ -1,3 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const H1 = ({ children }) => <h1>{children}</h1>
+const H1 = ({ children }) => <h1>{children}</h1>
+
+H1.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export { H1 }
--- src/stories/index.jsx
+++ src/stories/index.jsx
@@ -2,22 +2,28 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react'
 import { linkTo } from '@storybook/addon-links'
 
 import { Button, Welcome } from '@storybook/react/demo'
 
 import { H1 } from '../components/H1'
 
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-))
+// https://github.com/storybooks/storybook/tree/master/addons/knobs
+
+storiesOf('Welcome', module)
+  .addDecorator(withKnobs)
+  .add('to Storybook', () => <Welcome showApp={linkTo('Button')} />)
 
 storiesOf('Button', module)
+  .addDecorator(withKnobs)
   .add('with text', () => (
-    <Button onClick={action('clicked')}> Hello Button </Button>
+    <Button onClick={action('clicked')}>{text('Label', 'Hello World')}</Button>
   ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}> 😀😎👍💯 </Button>
   ))
 
-storiesOf('H1', module).add('basic usage', () => <H1> Headline! </H1>)
+storiesOf('H1', module)
+  .addDecorator(withKnobs)
+  .add('basic usage', () => <H1> Headline! </H1>)
