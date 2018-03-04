a0cadc77f3b45511bee9597d88e41fb259c6d835
Maximilian Stroh <Hisako1337@gmail.com>
2018-03-04 09:48:24 +0100

    include folder temporary

---
+++ dist/H2.js
@@ -0,0 +1,64 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.H2 = undefined;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var style = {
+  backgroundImage: 'linear-gradient(350deg,  #0d6abe 10%,#c99eca 100%)',
+  '-webkit-background-clip': 'text',
+  '-webkit-text-fill-color': 'transparent',
+  fontFamily: 'Tahoma, Geneva, sans-serif',
+  fontSize: 28,
+  marginBottom: 5
+};
+
+var styleInverse = {
+  color: 'white',
+  fontFamily: 'Tahoma, Geneva, sans-serif',
+  fontSize: 28,
+  marginBottom: 5,
+  textShadow: '0px 0px 1px #000'
+};
+
+var border = {
+  borderBottom: '1px solid #0d6abe'
+};
+
+var borderInverse = {
+  borderBottom: '1px solid white'
+};
+
+var H2 = function H2(_ref) {
+  var inverse = _ref.inverse,
+      children = _ref.children;
+  return _react2.default.createElement(
+    'div',
+    { style: inverse ? borderInverse : border },
+    _react2.default.createElement(
+      'h2',
+      { style: inverse ? styleInverse : style },
+      children
+    )
+  );
+};
+
+H2.propTypes = {
+  /** text or child element. shall not contain block-style elements */
+  children: _propTypes2.default.string.isRequired,
+  /** if enabled, renders white font. useful for dark/colored backgrounds */
+  inverse: _propTypes2.default.bool
+};
+
+exports.H2 = H2;
